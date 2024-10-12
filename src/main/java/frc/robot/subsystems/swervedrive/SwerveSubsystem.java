e8c948ceb6292a9d3e8044441cc24cd45a43cb98
H3ckboy <boggsc716@gmail.com>
2024-10-11 22:30:30 -0400

    add json files for swerve

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Swerve;
+package frc.robot.subsystems.swervedrive;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class SwerveModule extends SubsystemBase {
-  /** Creates a new SwerveModule. */
-  public SwerveModule() {}
+public class SwerveSubsystem extends SubsystemBase {
+  /** Creates a new SwerveSubsystem. */
+  public SwerveSubsystem() {}
 
   @Override
   public void periodic() {
